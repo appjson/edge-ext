aaf72e4372879bd349411be03945fe4fd1c96e2a
appjson <app.json@hotmail.com>
2022-07-21 21:05:10 +0800

    [update] 自述文件

--- README.md
+++ README.md
@@ -25,3 +25,13 @@ yarn install
 ```shell
 yarn build
 ```
+
+## 开发
+
+不使用 Typescript 编写可以直接修改`js`目录下的 Javascript 文件。
+
+使用 Typescript 开发：
+
+```shell
+tsc --watch
+```
--- js/background.js
+++ js/background.js
@@ -1,3 +1,8 @@
 chrome.runtime.onInstalled.addListener(async () => {
-    console.log("Extension started!");
+    console.log("%cExtension installed!", "font-size:24px;color:white;background-color:blue;padding:4px;");
+    chrome.contextMenus.create({
+        id: "save",
+        title: "记录：%s",
+        contexts: ["selection"],
+    });
 });
