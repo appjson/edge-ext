7d18911d0dbd534ece29ef63e7fa9b2a394bbc01
Saica.RC <zephyird@gmail.com>
2022-08-28 23:03:57 +0800

    调整样式，修复 antd 无动效问题

--- page-plugin/craco.config.js
+++ page-plugin/craco.config.js
@@ -8,7 +8,12 @@ module.exports = {
                 lessLoaderOptions: {
                     lessOptions: {
                         modifyVars: {
-
+                            'btn-primary-bg': '#C2E7FF',
+                            'border-radius-base': '8px',
+                            'btn-primary-color': '#1F4789',
+                            'btn-font-weight': 700,
+                            'btn-primary-shadow': '0px 4px 8px rgba(194, 231, 255, 0.5)',
+                            'popover-min-width': "100px",
                         },
                         javascriptEnabled: true,
                     },
