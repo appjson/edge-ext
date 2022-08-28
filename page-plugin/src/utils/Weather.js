3d72b50507a8b8c2fdd5c7f290d4c784b0984399
appjson <app.json@hotmail.com>
2022-08-28 20:57:39 +0800

    [update] 新增免费API KEY

--- page-plugin/src/utils/Weather.js
+++ page-plugin/src/utils/Weather.js
@@ -1,11 +1,22 @@
-const API_KEY = "4f5ea0930f7140d0a968d0627c16d9a1";
+const API_KEY_POOL = [
+  "4f5ea0930f7140d0a968d0627c16d9a1",
+  "f4bf39a5d1524d5db1b753f97a428caf",
+  "6dcbc0d27eb6437bae26f175876277b3",
+];
 
 const URL = "https://devapi.qweather.com/v7/weather/now";
 const GEO_URL = "https://geoapi.qweather.com/v2/city/lookup";
 
+/**
+ *
+ * @returns 随机API KEY
+ */
+const API_KEY = () =>
+  API_KEY_POOL[Math.floor(Math.random() * API_KEY_POOL.length)];
+
 async function getCity(position) {
   const resp = await fetch(
-    `${GEO_URL}?key=${API_KEY}&location=${position.coords.longitude.toFixed(
+    `${GEO_URL}?key=${API_KEY()}&location=${position.coords.longitude.toFixed(
       2
     )},${position.coords.latitude.toFixed(2)}`,
     {
@@ -39,7 +50,7 @@ export function getLocation() {
 }
 
 export async function getWeatherInfo(geostr) {
-  const resp = await fetch(`${URL}?key=${API_KEY}&location=${geostr}`, {
+  const resp = await fetch(`${URL}?key=${API_KEY()}&location=${geostr}`, {
     mode: "cors",
   });
   return await resp.json();
