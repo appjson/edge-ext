df3b7e768b0ad886538c39bd48bfe42383aa86ce
appjson <app.json@hotmail.com>
2022-08-31 00:03:33 +0800

    [feat] 新增静态猫猫切换

--- src/content/content.js
+++ src/content/content.js
@@ -1,4 +1,36 @@
 (function () {
+  function setupFlatPanel(catStyle) {
+    let canvas = document.createElement("img");
+    canvas.id = "img-edge-ext";
+    canvas.style.width = "150px";
+    canvas.style.height = "150px";
+    canvas.style.position = "fixed";
+    canvas.style.right = 0;
+    canvas.style.bottom = "50px";
+    canvas.style.pointerEvents = "none";
+    canvas.style.filter = "drop-shadow(0px 10px 10px #ccc)";
+    canvas.src = chrome.runtime.getURL(`/assets/flatcat/${catStyle}.png`);
+    document.body.appendChild(canvas);
+    const divElem = document.createElement("div");
+    divElem.id = "content-js-div-edge-ext";
+    divElem.style.position = "absolute";
+    divElem.style.right = "0";
+    divElem.style.bottom = "0";
+    divElem.style.pointerEvents = "none";
+    document.body.appendChild(divElem);
+    const reactScript = document.createElement("script");
+    reactScript.setAttribute("type", "text/javascript");
+    reactScript.src = chrome.runtime.getURL("/assets/react-edge-ext.js");
+    const styleCss = document.createElement("link");
+    styleCss.setAttribute("rel", "stylesheet");
+    styleCss.setAttribute(
+      "href",
+      chrome.runtime.getURL("/assets/content-edge-ext.css")
+    );
+    document.head.appendChild(reactScript);
+    document.head.appendChild(styleCss);
+  }
+
   function setupCatPanel() {
     let canvas = document.createElement("canvas");
     canvas.id = "live2d-edge-ext";
@@ -11,7 +43,6 @@
     canvas.style.bottom = 0;
     canvas.style.pointerEvents = "none";
     canvas.style.filter = "drop-shadow(0px 10px 10px #ccc)";
-
     document.body.appendChild(canvas);
     const divElem = document.createElement("div");
     divElem.id = "content-js-div-edge-ext";
@@ -41,7 +72,7 @@
     let meta = document.createElement("meta");
     meta.setAttribute(
       "content",
-      chrome.runtime.getURL(`/assets/${color}cat/model.json`)
+      chrome.runtime.getURL(`/assets/${color}/model.json`)
     );
     meta.setAttribute("name", "model_url");
     document.head.appendChild(meta);
@@ -58,13 +89,15 @@
     document.head.appendChild(temp);
   }
 
-  chrome.storage.sync.get(["catShow", "catColor"], (res) => {
-    const catShow = res.catShow;
-    const catColor = res.catColor === true ? "white" : "black";
-    if (catShow) {
+  chrome.storage.sync.get(["catShow", "catStyle"], (res) => {
+    const catShow = res.catShow || true;
+    const catStyle = res.catStyle || "cat-1";
+    if (catShow && catStyle !== "blackcat" && catStyle !== "whitecat") {
+      setupFlatPanel(catStyle);
+    } else if (catShow) {
       injectCustomJs("/assets/live2d-mini.js", function () {
         setupCatPanel();
-        setupModel(catColor);
+        setupModel(catStyle);
       });
     }
   });
--- src/popup/PopUp.vue
+++ src/popup/PopUp.vue
@@ -1,28 +1,69 @@
 <script setup>
 import { ref, watch, onMounted } from "vue";
-import { NSpace, NSwitch, NCollapseTransition, NButton } from "naive-ui";
+import {
+  NSpace,
+  NSwitch,
+  NCollapseTransition,
+  NButton,
+  NDropdown,
+  NTag,
+  NDivider,
+} from "naive-ui";
 
-// const timer = ref("");
-// const msg = ref("");
+const options = [
+  {
+    label: "静态猫猫",
+    key: "flat",
+    children: [
+      {
+        label: "cat-1",
+        key: "cat-1",
+      },
+      {
+        label: "cat-2",
+        key: "cat-2",
+      },
+      {
+        label: "cat-3",
+        key: "cat-3",
+      },
+      {
+        label: "cat-4",
+        key: "cat-4",
+      },
+      {
+        label: "cat-5",
+        key: "cat-5",
+      },
+      {
+        label: "cat-6",
+        key: "cat-6",
+      },
+    ],
+  },
+  {
+    label: "动态猫猫",
+    key: "live",
+    children: [
+      {
+        label: "White Cat",
+        key: "whitecat",
+      },
+      {
+        label: "Black Cat",
+        key: "blackcat",
+      },
+    ],
+  },
+];
 
-// function startTimer() {
-//   const time = Number.parseInt(timer.value);
-//   if (time >= 0) {
-//     chrome.runtime.sendMessage("", {
-//       type: "timer",
-//       title: msg.value,
-//       time: timer.value,
-//     });
-//   }
-// }
-
+const arr = options[0].children.concat(options[1].children);
 const showBtn = ref(false);
-const showColor = ref(true);
 
 onMounted(() => {
-  chrome.storage.sync.get(["catShow", "catColor"], (res) => {
+  chrome.storage.sync.get(["catShow", "catStyle"], (res) => {
     showBtn.value = res.catShow;
-    showColor.value = res.catColor;
+    handleSelect(res.catStyle);
   });
 });
 
@@ -32,11 +73,14 @@ watch(showBtn, (newVal) => {
   });
 });
 
-watch(showColor, (newVal) => {
-  chrome.storage.sync.set({ catColor: newVal }, () => {
-    console.log("Cat Color:", newVal);
+const selected = ref("");
+const handleSelect = function (key) {
+  chrome.storage.sync.set({ catStyle: key }, () => {
+    console.log("Cat Style:", key);
   });
-});
+  const label = arr.filter((v) => v.key === key);
+  selected.value = label.length === 1 ? label[0].label : "未选择";
+};
 
 const reload = function () {
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
@@ -47,23 +91,33 @@ const reload = function () {
 
 <template>
   <div class="section">
-    <div class="label">宠物开关</div>
     <n-space vertical>
+      <div class="label">宠物开关</div>
       <n-switch v-model:value="showBtn">
         <template #checked> 开启 </template>
         <template #unchecked> 关闭 </template>
       </n-switch>
       <n-collapse-transition :show="showBtn">
-        <div class="label">宠物颜色</div>
-        <n-switch v-model:value="showColor">
-          <template #checked> 白色 </template>
-          <template #unchecked> 黑色 </template>
-        </n-switch>
+        <div class="label">宠物样式</div>
+        <n-dropdown
+          trigger="hover"
+          size="small"
+          :options="options"
+          :show-arrow="true"
+          @select="handleSelect"
+        >
+          <n-button>选择样式</n-button>
+        </n-dropdown>
+        <n-divider vertical />
+        当前选择：<n-tag :bordered="false" type="success">
+          {{ selected }}
+        </n-tag>
       </n-collapse-transition>
-      <n-button strong secondary round type="info" @click="reload">
-        应用
-      </n-button>
     </n-space>
+    <n-divider />
+    <n-button strong secondary round type="info" @click="reload">
+      应用
+    </n-button>
   </div>
 </template>
 
