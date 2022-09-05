bd8e47c3d3e27fdc29cd9ae081e25b6116dc6ed1
SunLine-THU <93499895+SunLine-THU@users.noreply.github.com>
2022-09-05 12:41:11 +0800

    上传了一个疫情新闻的api请求格式
    通过原生js请求疫情api，并通过对页面元素进行写入来实现新闻展示效果。

--- src/news.js
+++ src/news.js
@@ -16,7 +16,7 @@ function nCoV_news(){
 			// 将对指定内容添加入页面元素中
 			// 其中0表示第一条新闻，如果需要更多新闻请使用循环体for(i = 0;i < obj['results'].length;i ++)，或者手动指定显示第几条新闻
 			content.innerHTML="消息来源:<br />" + obj['results'][0]['infoSource'] + "<br />新闻标题:<br />" + obj['results'][0]['title'] + "<br />新闻内容:<br />" + obj['results'][0]['summary'];
-		}
+		} 
 	};
 	// 获取疫情新闻api的地址，以及传入特定参数例如page和num
 	xhttp.open("GET", "https://lab.isaaclin.cn/nCoV/api/news?page=1&num=20", true);
